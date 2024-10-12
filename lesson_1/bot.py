ff9a9fe6e36b480d6522d22ddd599394ecf6ef2d
Abdykadyrov-S <abdykadyrovsyimyk0708@gmail.com>
2024-10-12 19:59:40 +0600

    lesson 1

---
+++ lesson_1/bot.py
@@ -0,0 +1,70 @@
+import asyncio # встроенный модуль для работы с асинхронностью
+import logging
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import CommandStart, Command
+
+from config import token
+
+"""
+Bot - Абстрактный класс для того что бы создать объект класса
+
+Dispatcher - посредник 
+
+types - модуль в aiogram
+
+CommandStart - класс для создания комманды start
+
+Command - класс для создания комманд
+"""
+
+
+bot = Bot(token=token)
+
+dp = Dispatcher()
+
+"""
+@dp.message - хендлер (обработчик сообщений)
+
+
+"""
+
+
+@dp.message(CommandStart()) 
+# /start == сообщению который отправил пользователь
+# if True:
+    # start()
+# else:
+    # pass
+async def start(message: types.Message):
+    await message.answer("Привет")
+
+@dp.message(Command('help'))
+async def help_comm(message: types.Message):
+    await message.reply('Чем могу помочь?')
+
+@dp.message(Command('play'))
+async def help_comm(message: types.Message):
+    await message.answer_photo('https://media.makeameme.org/created/you-win-nothing-b744e1771f.jpg', caption="Вы выиграли")
+
+@dp.message(F.text == "Привет")
+async def echo(message: types.Message):
+    await message.answer("Привет, как дела?")
+
+@dp.message(F.text.in_({"Привет", 'привет', "hi", "как дела"}))
+async def echo(message: types.Message):
+    await message.answer("Привет, как дела?")
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer("Я вас не понял")
+
+
+async def main():
+    logging.basicConfig(level="INFO")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Exit")
